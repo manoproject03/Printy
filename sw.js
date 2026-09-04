186117b10bef3433766cc623a016e40e82be76dd
Manar <130401137+manoproject03@users.noreply.github.com>
2026-09-04 23:55:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'printy-cache-v1';
-const CORE_ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CORE_ASSETS = ['./index.html', './manifest.json', './1788540620065.png', './1788540620065.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
